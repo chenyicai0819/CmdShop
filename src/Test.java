6f565c9dc06b8e8836e8538e74ccfc842e7555ea
chenyicai0819 <2495842975@qq.com>
2021-01-06 23:50:43 +0800

    Test代码优化，去除循环外的购买商品代码

--- src/Test.java
+++ src/Test.java
@@ -33,34 +33,8 @@ public class Test {
                     /*
                     登陆成功后显示商品目录
                      */
-                    System.out.println("以下是商城中的商品");
-                    ReadProductExcel readProductExcel = new ReadProductExcel();
-                    Product products[] = readProductExcel.getAllExcel(inProduct);
-                    for (Product product : products) {
-                        System.out.print(product.getpID());
-                        System.out.print("\t" + product.getpName());
-                        System.out.print("\t" + product.getPrice());
-                        System.out.println("\t" + product.getpDesc());
-                    }
-
-                    System.out.println("请输入想要购买的商品ID添加商品到购物车");
-                    String pID = sc.next();
-
-                    /*
-                    创建一个购物车
-                     */
                     int count = 0;
                     Product carts[] = new Product[3];
-                    /*
-                    查找是否有该商品信息，有的话就返回商品信息
-                     */
-                    inProduct = null;
-                    inProduct = Class.forName("Test").getResourceAsStream("/product.xlsx");
-                    Product product = readProductExcel.getProductById(pID, inProduct);
-                    System.out.println("所购买的商品的价格为" + product.getPrice());
-                    if (product != null) {
-                        carts[count++] = product;
-                    }
 
 
                     int buying = 1;
@@ -69,14 +43,14 @@ public class Test {
                         System.out.println("查看购物车请按2");
                         System.out.println("结账请按3");
                         int choose = sc.nextInt();
-                        int panduan = 1;
+
                         if (choose == 1) {
 
                             inProduct = null;
                             inProduct = Class.forName("Test").getResourceAsStream("/product.xlsx");
                             System.out.println("以下是商城中的商品");
-                            readProductExcel = new ReadProductExcel();
-                            products = readProductExcel.getAllExcel(inProduct);
+                            ReadProductExcel readProductExcel = new ReadProductExcel();
+                            Product products[] = readProductExcel.getAllExcel(inProduct);
                             for (Product newproduct : products) {
                                 System.out.print(newproduct.getpID());
                                 System.out.print("\t" + newproduct.getpName());
@@ -84,14 +58,15 @@ public class Test {
                                 System.out.println("\t" + newproduct.getpDesc());
                             }
                             System.out.println("请输入想要购买的商品ID添加商品到购物车");
-                            pID = sc.next();
+                            String pID = sc.next();
                             inProduct = null;
                             inProduct = Class.forName("Test").getResourceAsStream("/product.xlsx");
-                            product = readProductExcel.getProductById(pID, inProduct);
+                            Product product = readProductExcel.getProductById(pID, inProduct);
                             System.out.println("所购买的商品的价格为" + product.getPrice());
                             if (product != null) {
                                 carts[count++] = product;
                             }
+
                         } else if (choose == 2) {
                             System.out.println("这是您的购物车");
                             for (Product gouwuche : carts) {
